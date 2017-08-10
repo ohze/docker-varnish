a1a88057fa5f00abd9f667667a403b12461be811
thanhbv <thanhbv@sandinh.net>
2017-08-10 21:06:18 +0700

    check MEMLOCK `ulimit -l`

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -x
 
-[ -z "$VARNISH_SECRET" ] || echo ${VARNISH_SECRET} > /etc/varnish/secret
+if [ "${1}" = "varnishd" ]; then
+    [ -z "$VARNISH_SECRET" ] || echo ${VARNISH_SECRET} > /etc/varnish/secret
 
-if [ `ulimit -n` -lt 131072 ]; then
-    echo "\
-'ulimit -n' = `ulimit -n` is way too small for varnish.\n\
-Pls run docker with '--ulimit nofile=131072:131072' option"
-    exit
+    if [ `ulimit -n` -lt 131072 ] || [ `ulimit -l` -lt 82000 ]; then
+        echo "\
+ulimit NFILES or MEMLOCK is way too small for varnish.\n\
+Pls run docker with '--ulimit nofile=131072:131072 --ulimit memlock=83968000' options"
+        exit
+    fi
 fi
-
 exec "$@"
