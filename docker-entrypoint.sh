5e01806a680ecfcfb2ebcae65c71608c97fbcd48
thanhbv <thanhbv@sandinh.net>
2017-08-13 15:27:33 +0700

    set ulimit if docker is run with --cap-add SYS_RESOURCE

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
-set -x
 
 if [ "${1}" = "varnishd" ]; then
     [ -z "$VARNISH_SECRET" ] || echo ${VARNISH_SECRET} > /etc/varnish/secret
 
-    if [ `ulimit -n` -lt 131072 ] || [ `ulimit -l` -lt 82000 ]; then
-        echo "\
-ulimit NFILES or MEMLOCK is way too small for varnish.\n\
-Pls run docker with '--ulimit nofile=131072:131072 --ulimit memlock=83968000' options"
-        exit
+    if [ `ulimit -n` -lt 131072 ]; then
+        ulimit -n 131072
+        if [ $? -ne 0 ]; then
+            echo "\
+ulimit -n = `ulimit -n` is way too small for varnish\n\
+Pls run docker with '--ulimit nofile=131072:131072' or '--cap-add SYS_RESOURCE' option"
+            exit
+        fi
+    fi
+    if [ `ulimit -l` -lt 82000 ]; then
+        ulimit -l 82000
+        if [ $? -ne 0 ]; then
+            echo "\
+ulimit -l = `ulimit -l` is way too small for varnish.\n\
+Pls run docker with '--ulimit memlock=83968000' or '--cap-add SYS_RESOURCE' option"
+            exit
+        fi
     fi
 fi
+
 exec "$@"
